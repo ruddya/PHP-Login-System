75d2d8579872b4e9405ec88f3ff71ed4ff5ee49b
Amanda J.R. Perez <ajr.perez@outlook.com>
2021-08-20 15:38:48 -0400

    Added login form

--- index.php
+++ index.php
@@ -1,1 +1,36 @@
-<?php echo "Hewwo Wowld"; ?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Title</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="css/uikit.min.css" />
+        <script src="js/uikit.min.js"></script>
+        <script src="js/uikit-icons.min.js"></script>
+    </head>
+    <body>
+
+    	<div class="uk-section uk-container">
+    		<div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+    			<form class="uk-form-stacked js-login">
+				    <div class="uk-margin">
+				        <label class="uk-form-label" for="form-stacked-text">Email</label>
+				        <div class="uk-form-controls">
+				            <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+				        </div>
+				    </div>
+				    <div class="uk-margin">
+				        <label class="uk-form-label" for="form-stacked-text">Password</label>
+				        <div class="uk-form-controls">
+				            <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="password">
+				        </div>
+				    </div>
+				    <div class="uk-margin">
+				        <button class="uk-button uk-button-default" type="submit">Login</button>
+				    </div>
+				</form>
+    		</div>
+    	</div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    </body>
+</html>
