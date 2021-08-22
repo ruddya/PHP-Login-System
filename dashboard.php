a21dec4a2749dafdb39cea9249020b9ec262eb2a
Amanda J.R. Perez <ajr.perez@outlook.com>
2021-08-22 19:20:06 -0400

    Added dashboard page

---
+++ dashboard.php
@@ -0,0 +1,25 @@
+<?php 
+	//allow the config
+	define('__CONFIG__', true);
+	//require config
+	require_once "inc/config.php"; 
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Login System</title>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" href="assets/css/uikit.min.css" />
+        <script src="assets/js/uikit.min.js"></script>
+        <script src="assets/js/uikit-icons.min.js"></script>
+    </head>
+    <body>
+
+    	<div class="uk-section uk-container">
+
+    	</div>
+    	<?php require_once "inc/footer.php"; ?>
+    </body>
+</html>
